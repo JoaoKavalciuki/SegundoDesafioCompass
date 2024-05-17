503ecd3c21e76fa4962ba8378a055f74e3f9cd9e
magrininicolas <magrininicolas@outlook.com>
2024-05-17 14:15:29 -0300

    Adicionado anotação para id em Item

--- src/main/java/org/example/entities/Item.java
+++ src/main/java/org/example/entities/Item.java
@@ -4,6 +4,9 @@ import java.io.Serializable;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
@@ -11,6 +14,8 @@ import jakarta.persistence.Table;
 public class Item implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String categoria;
     @Column(name = "item")
