12a7f5b739f626f7b1cd07a1826b1905cf6d1c72
Edwin Yassunaga <163698976+EdYass@users.noreply.github.com>
2024-05-20 23:40:45 -0300

    Delete SegundoDesafioCompass-Abrigo directory

--- SegundoDesafioCompass-Abrigo/src/main/java/org/example/entity/enums/StatusPedido.java
+++
@@ -1,9 +0,0 @@
-package org.example.entity.enums;
-
-public enum StatusPedido {
-
-    PENDENTE,
-    ACEITO,
-    RECUSADO
-
-}
--- SegundoDesafioCompass-Abrigo/src/main/java/org/example/view/View.java
+++
@@ -1,5 +0,0 @@
-package org.example.view;
-
-public interface View {
-    void display();
-}
