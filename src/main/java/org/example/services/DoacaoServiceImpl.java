42003fa3fab9ea1d1f72983059d89ccbdaae4368
magrininicolas <magrininicolas@outlook.com>
2024-05-22 17:20:19 -0300

    CRUD quase finalizado

--- src/main/java/org/example/services/DoacaoServiceImpl.java
+++ src/main/java/org/example/services/DoacaoServiceImpl.java
@@ -1,5 +1,7 @@
 package org.example.services;
 
+import java.util.List;
+
 import org.example.entities.Doacao;
 import org.example.repositories.DoacaoRepository;
 import org.example.services.interfaces.DoacaoService;
@@ -15,16 +17,28 @@ public class DoacaoServiceImpl implements DoacaoService {
         var total = doacaoRepository.calcularQuantidadeTotal(doacao.getItem(), doacao.getCentroDistribuicao());
         if (total + doacao.getQuantidade() > 1000) {
             System.out.println(
-                    "O centro não pode armazenar mais do que 1000 itens deste tipo. Quantidade Atual: " + total);
+                    "O centro não pode armazenar mais do que 1000 itens do tipo \"" + doacao.getItem().getCategoria()
+                            + "\". Quantidade Atual: " + total);
             return true;
         }
         return false;
     }
 
+    @Override
     public void save(Doacao doacao) {
         if (verificaQuantidadeTotal(doacao))
             return;
         doacaoRepository.save(doacao);
         System.out.println("Doação enviada ao centro!");
+    }
+
+    @Override
+    public List<Doacao> listAll() {
+        return doacaoRepository.listAll();
+    }
+
+    @Override
+    public List<Doacao> listByCategoria(String categoria) {
+        return doacaoRepository.listByCategoria(categoria);
     }
 }
