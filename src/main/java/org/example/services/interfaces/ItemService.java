361517ef056af298adc452225917d47ddf62328d
magrininicolas <magrininicolas@outlook.com>
2024-05-22 17:20:44 -0300

    CRUD quase finalizado

--- src/main/java/org/example/services/interfaces/ItemService.java
+++ src/main/java/org/example/services/interfaces/ItemService.java
@@ -7,5 +7,15 @@ import org.example.entities.Item;
 public interface ItemService {
     List<Item> findAll();
 
+    List<Item> findByCategoria(String categoria);
+
     Item findById(Long id);
+
+    Item findByName(String name);
+
+    void save(Item item);
+
+    void update(String novoNome, Long id);
+
+    void deleteById(Long id);
 }
