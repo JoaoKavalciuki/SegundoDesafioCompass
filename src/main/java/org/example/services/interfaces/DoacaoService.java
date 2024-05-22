407a83e3ba653a8615b5b6ead72368076e3a216b
magrininicolas <magrininicolas@outlook.com>
2024-05-22 17:20:36 -0300

    CRUD quase finalizado

--- src/main/java/org/example/services/interfaces/DoacaoService.java
+++ src/main/java/org/example/services/interfaces/DoacaoService.java
@@ -1,7 +1,13 @@
 package org.example.services.interfaces;
 
+import java.util.List;
+
 import org.example.entities.Doacao;
 
 public interface DoacaoService {
     void save(Doacao doacao);
+
+    List<Doacao> listAll();
+
+    List<Doacao> listByCategoria(String categoria);
 }
